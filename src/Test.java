c0866afdef2a644148625d8e62736113410e0272
AbdulrahmanAlwabil <abdulrahmanalialwabil@gmail.com>
2024-11-15 20:48:55 +0300

    FINISHED WITH SEARCH ENGINE

--- src/Test.java
+++ src/Test.java
@@ -1,21 +1,12 @@
-
+import javax.swing.JFrame;
 public class Test {
     public static void main(String[] args) {
 
-        TextProccesor tp = new TextProccesor();
-        tp.fetchData((LinkedList<String>)tp.fetchStopWords());
-        
-        SearchEngine se = new SearchEngine();
-        AVL<String> avl = se.rankedSearchAVL("market sports");
-        
-        
-        
-        LinkedPQ pq = avl.makePQ();
-        pq.print();
-
-
-        // AVL<String> avl = se.querySearch("market OR sports AND warming");
-        // avl.print();
+        JFrame frame = new JFrame();
+        frame.setSize(600, 400);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 }
