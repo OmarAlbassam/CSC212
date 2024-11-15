313b548d73851066936414b441d0fd2cf542e7c5
3monn <omar59056@gmail.com>
2024-11-16 00:21:26 +0300

    Finished Ranked Retrieval GUI

--- src/LinkedPQ.java
+++ src/LinkedPQ.java
@@ -1,59 +1,80 @@
 public class LinkedPQ {
-    private int size;
-    private PQNode head;
+	private int size;
+	private PQNode head;
 
+	/* tail is of no use here. */
+	public LinkedPQ() {
+		head = null;
+		size = 0;
+	}
 
-    /* tail is of no use here. */
-    public LinkedPQ() {
-        head = null;
-        size = 0;
-    }
+	public int length() {
+		return size;
+	}
 
-    public int length (){
-        return size;
-    }
+	public boolean full() {
+		return false;
+	}
 
+	public void enqueue(String e, int pty) {
+		PQNode tmp = new PQNode(e, pty);
+		if ((size == 0) || (pty > head.priority)) {
+			tmp.next = head;
+			head = tmp;
+		} else {
+			PQNode p = head;
+			PQNode q = null;
+			while ((p != null) && (pty <= p.priority)) {
+				q = p;
+				p = p.next;
+			}
+			tmp.next = p;
+			q.next = tmp;
+		}
+		size++;
+	}
 
+	public PQElement serve() {
+		PQNode node = head;
+		PQElement pqe = new PQElement(node.data, node.priority);
+		head = head.next;
+		size--;
+		return pqe;
+	}
 
-    public boolean full () {
-        return false;
-    }
+	public void print() {
+		PQNode tmp = head;
+		for (int i = 0; i < length(); i++) {
+			if (tmp.next != null)
+				System.out.print(tmp.data + ":" + tmp.priority + " -> ");
+			else
+				System.out.println(tmp.data + ":" + tmp.priority);
+			tmp = tmp.next;
+		}
+	}
 
-    public void enqueue(String e, int pty) {
-        PQNode tmp = new PQNode(e, pty);
-        if((size == 0) || (pty > head.priority)) {
-            tmp.next = head;
-            head = tmp;
-        }
-        else {
-            PQNode p = head;
-            PQNode q = null;
-            while((p != null) && (pty <= p.priority)) {
-                q = p;
-                p = p.next;
-            }
-            tmp.next = p;
-            q.next = tmp;
-        }
-        size++;
-    }
+	public String Result() {
+		String result = "";
+		PQNode tmp = head;
+		int j = 0;// to add \n after 8 iterations so it fills the lines
+		for (int i = 0; i < length(); i++) {
+			if (tmp.next != null) {
+				result += "( "+tmp.data + " : " + tmp.priority+" ) ";
+				if (j == 7) {
+					result += "\n";
+					j = 0;
+				} else 
+					j++;
+				
+			} else {
+				result += "( "+tmp.data + " : " + tmp.priority+" ) "; // Append with a newline
+			}
 
-    public PQElement serve(){
-        PQNode node = head;
-        PQElement pqe=new PQElement(node.data,node.priority);
-        head = head.next;
-        size--;
-        return pqe;
-    }
+			tmp = tmp.next;
+		}
+		if (result.isEmpty())
+			result = "Not Found";
+		return result;
+	}
 
-    public void print() {
-        PQNode tmp = head;
-        for (int i = 0; i < length(); i++) {
-            if (tmp.next != null)
-                System.out.print(tmp.data + ":" + tmp.priority +  " -> ");
-            else 
-                System.out.println(tmp.data + ":" + tmp.priority);
-            tmp = tmp.next;
-        }
-    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,59 @@
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-public class Test {
-    public static void main(String[] args) {
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
+public class Test {
+    static SearchEngine s = new SearchEngine();
+
+    public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setSize(600, 400);
         frame.setResizable(false);
+        frame.setLayout(null);
+
+        JTextField inputField = new JTextField();
+        inputField.setBounds(150, 20, 300, 30);
+        frame.add(inputField);
+
+        JTextArea outputField = new JTextArea();
+        outputField.setFont(new Font("Arial", Font.BOLD, 16));
+        outputField.setEditable(false);
+        outputField.setBackground(Color.white);
+        outputField.setLineWrap(false);
+        outputField.setWrapStyleWord(false);
+
+        JScrollPane scrollPane = new JScrollPane(outputField);
+        scrollPane.setBounds(50, 140, 500, 200);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+        frame.add(scrollPane);
+
+        JButton booleanButton = new JButton("Boolean Retrieval");
+        booleanButton.setBounds(120, 60, 150, 50);
+        frame.add(booleanButton);
+
+        JButton rankedButton = new JButton("Ranked Retrieval");
+        rankedButton.setBounds(320, 60, 150, 50);
+        rankedButton.addActionListener(e -> {
+            String input = inputField.getText();
+            String result = rankedRetievalAction(input);
+            outputField.setText(result);
+        });
+        frame.add(rankedButton);
+
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
+    public static String rankedRetievalAction(String in) {
+        String input = in;
+        AVL<String> out = s.rankedSearchAVL(input);
+        LinkedPQ pq = out.makePQ();
+        String res = pq.Result();
+        return res;
     }
 }
